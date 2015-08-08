16fbeb668b369c2795ed89e7fcc2c5e2b4d5ffd2
thedevilking <thedevilking@qq.com>
2015-08-08 21:14:15 +0800

    2015.8.8 -wwtao

--- src/android/geosvr/dtn/servlib/geohistorydtn/test/Test_timeManager.java
+++ src/android/geosvr/dtn/servlib/geohistorydtn/test/Test_timeManager.java
@@ -1,6 +1,13 @@
 package android.geosvr.dtn.servlib.geohistorydtn.test;
 
 import android.geosvr.dtn.servlib.geohistorydtn.area.Area;
+import android.geosvr.dtn.servlib.geohistorydtn.frequencyVector.FrequencyVector;
+import android.geosvr.dtn.servlib.geohistorydtn.frequencyVector.FrequencyVectorLevel;
+import android.geosvr.dtn.servlib.geohistorydtn.frequencyVector.FrequencyVectorManager;
+import android.geosvr.dtn.servlib.geohistorydtn.frequencyVector.FrequencyVectorServiceType;
+import android.geosvr.dtn.servlib.geohistorydtn.frequencyVector.HourFrequencyVector;
+import android.geosvr.dtn.servlib.geohistorydtn.timeManager.TimeManager;
+import android.widget.SlidingDrawer;
 import junit.framework.TestCase;
 
 /** 
@@ -14,7 +21,7 @@ public class Test_timeManager extends TestCase{
 	 * 对区域的向量计时进行测试
 	 * 区域，0.101.10.1 -> 0.102.10.1 -> 0.113.11.1 -> 0.214.21.2 -> 0.101.10.1  
 	 */
-	
+	/*
 	public void testAreaTimeCount()
 	{
 		//第一层区域
@@ -30,6 +37,47 @@ public class Test_timeManager extends TestCase{
 		
 		//第三层区域
 		Area area1=new Area(3, 1);
-		Area area2=new Area(2, 2);
+		Area area2=new Area(3, 2);
+		
+		//区域移动
+		area101.addTimeCount();
+//		area101.
+		double[] vector=area101.getFrequencyVectorList().get(0).getVector();
+		String s="";
+		for(int i=0;i<vector.length;i++)
+			s+=String.valueOf(vector[i])+",";
+		System.out.println("vector:"+s);
+		
+		try {
+			Thread.sleep(50000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		s="";
+		vector=area101.getFrequencyVectorList().get(0).getVector();
+		for(int i=0;i<vector.length;i++)
+			s+=String.valueOf(vector[i])+",";
+		System.out.println("vector:"+s);
+	}
+	*/
+	public void testTimeManager()
+	{
+		TimeManager tmanager=TimeManager.getInstance();
+		FrequencyVector vector=FrequencyVectorManager.getInstance().newFVector(FrequencyVectorLevel.hourVector,FrequencyVectorServiceType.AREA);
+		tmanager.addVectorListen(vector);
+		
+		int seconds=0;
+		for(;seconds<600;seconds++)
+		{
+			System.out.println("第"+seconds+"秒，vector:  "+vector.toString());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
